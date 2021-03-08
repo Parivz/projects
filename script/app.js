1ee99f34fbf723787ddea8bef1fda19e49269942
IT <n.parivz.b@yandex.ru>
2021-03-08 10:14:22 +0500

    first commit

---
+++ script/app.js
@@ -0,0 +1,13 @@
+// 1 Переменные
+const name = 'Parviz'
+const firstName = 'Negmatov'
+const age = 26
+
+// 2 Мутирование
+//console.log('Возрост человека: ' + age + ' лет')
+
+const lastName = prompt(message, "Введите фамилию")
+alert(firstName + ' ' + lastName)
+
+
+
